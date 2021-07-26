78eb2d0197568794ff45e5419894054bf5fd5f32
JESUS ADRIAN LLANOS JIMENEZ <a17300148@live.ceti.mx>
2021-07-26 12:02:54 -0500

    Solucion vistas

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.base;
     requires java.net.http;
     requires com.google.gson;
     requires org.controlsfx.controls;
